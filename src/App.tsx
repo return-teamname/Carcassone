e449322066e48cfe54906f5bf7a7c31154b47f5b
ezyyeah <52110827+ezyyeah@users.noreply.github.com>
2023-04-03 11:30:13 +0200

    tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 
 function App() {
+
+  // map variables
+  const gridSize = 64;
+  const mapWidth = 8;
+  const mapHeight = 5;
+
+  const calculatePlacement = (item: string, x: number, y: number) => {
+    // - - - - - - -
+    // - - - - - - -
+    // - - - - - - -
+    // - - - - - - -
+    // - - - - - - -
+
+    // for (var i = 0; i < mapWidth; i++) {
+    //   for (var j = 0; j < mapHeight; j++) {
+    //     var xPos: number = i * gridSize;
+    //     var yPos: number = j * gridSize;
+
+
+    //   }
+    // }
+  }
+
   return (
     <>
-      
+
     </>
   );
 }
